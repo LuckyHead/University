8074d923f0d14188eac0a62655ceadfe5b743317
LuckyHead <azizbeksaparniyazov5@gmail.com>
2024-02-23 17:47:46 +0500

    update2

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,7 +4,10 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'production.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LuckyHead$default',
+        'USER': 'LuckyHead',
+        'PASSWORD': 'qwerty10051983',
+        'HOST': 'LuckyHead.mysql.pythonanywhere-services.com',
     }
 }
